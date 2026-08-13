cc64e0f50e02a47ed12700e010c89479d0ece7cf
Ruthik H <ruthikgowda401@gmail.com>
2026-08-13 16:42:21 +0530

    DAY -3 new

---
+++ 485.py
@@ -0,0 +1,12 @@
+#count the max consecutive 1's in an array 
+nums=[1,1,0,1,1,1]
+count = 0
+max_count=0
+for i in nums:
+    if i==1:
+        count+=1
+        if count>max_count:
+            max_count=count
+    else:
+        count=0
+print(max_count)
