29731a6012b23356710c83c36b2f944cf5a00d64
Ruthik H <ruthikgowda401@gmail.com>
2026-08-15 21:55:20 +0530

    Day-4

---
+++ 26.py
@@ -0,0 +1,22 @@
+# #Remove Duplicates from Sorted Array
+nums = [1,1,2,2,2,3,4,5,5,4,7,7]
+# target=[]
+# k=0
+# for i in range(len(nums)):
+#     if nums[i] not in target:
+#         target.append(nums[i])
+#         k+=1
+# for i in range(k):
+#     nums[i]=target[i]
+# print(k)
+# print(target)
+a=set(nums)
+print(a)
+
+
+        
+
+            
+
+
+    
