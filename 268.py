dcc2ea4f9ef04890d1a60d95d633ccbae842e94d
Ruthik H <ruthikgowda401@gmail.com>
2026-08-17 20:20:09 +0530

    DAY 7

---
+++ 268.py
@@ -0,0 +1,13 @@
+# missing number 
+nums=[3,0,1]
+for i in range(len(nums)+1): # here we have added +1 because len(nums) goes until 4 but range stops one step before 
+    if i not in nums:
+        print(i)
+
+    
+   
+    
+        
+    
+
+
