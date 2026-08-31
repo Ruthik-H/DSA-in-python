f0705fa08205be9d41a4fcc0521bba54e790d1ef
Ruthik H <ruthikgowda401@gmail.com>
2026-08-31 18:03:06 +0530

    pascal triangle

--- 118-Pascal's triangle.py
+++ 118-Pascal's triangle.py
@@ -1,3 +1,8 @@
-numRows=5
-m=[[0] for _ in range(numRows)]
-print(m)
+numRows = 5
+result=[]
+for i in range(numRows):
+    row=[1]*(i+1)
+    for j in range(1,i):
+        row[j]=result[i-1][j-1]+result[i-1][j]
+    result.append(row)
+print(result)
