6b75c6faa2d0616917ff730dfd60306d593ffb69
Ruthik H <ruthikgowda401@gmail.com>
2026-09-10 22:21:11 +0530

    ROMAN to STring

---
+++ strings/13.py
@@ -0,0 +1,19 @@
+# Roman To Integer
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+s='LVIII'
+total=0
+for i in range(len(s)-1):
+    if values[s[i]]<values[s[i+1]]:
+        total-=values[s[i]]
+    else:
+        total+=values[s[i]]
+total+=values[s[-1]]
+print(total)
