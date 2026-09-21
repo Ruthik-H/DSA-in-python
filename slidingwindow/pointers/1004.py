5b574d5075c5f8e0da3585fada17d302765a47a2
Ruthik H <ruthikgowda401@gmail.com>
2026-09-21 22:32:01 +0530

    sliding window

--- slidingwindow/pointers/1004.py
+++ slidingwindow/pointers/1004.py
@@ -28,3 +28,23 @@ for i in range(len(nums)):
     max_count = max(count, max_count)
 
 print(max_count)
+
+
+
+
+
+
+
+# class Solution:
+#     def longestOnes(self, nums: list[int], k: int) -> int:
+#         left=0
+#         max_count=0
+#         for right in range(len(nums)):
+#             if nums[right]==0:
+#                 k-=1
+#             while k<0:
+#                 if nums[left]==0:
+#                     k+=1
+#                 left+=1
+#             max_count=max(max_count,right-left+1)
+#         return max_count
